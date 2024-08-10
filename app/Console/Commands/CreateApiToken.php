89b5edb3610fdbee7749a7d3ac3c3a1504506174
Maksym Kovalenko <maxeladze@gmail.com>
2024-08-10 15:39:29 +0200

    add user token creation command

---
+++ app/Console/Commands/CreateApiToken.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CreateApiToken extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'api:token {email} {token_name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new API token for a user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user = User::where('email', $this->argument('email'))->first();
+
+        if (!$user) {
+            $this->error('User not found');
+            return 1;
+        }
+
+        $token = $user->createToken($this->argument('token_name'));
+
+        $this->info("API token created successfully: " . $token->plainTextToken);
+        return 0;
+    }
+}
